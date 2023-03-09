acaeec978983c9d2cf65b667c29100f3cb4b8a7b
ducthuy-ng <46134513+ducthuy-ng@users.noreply.github.com>
2023-03-09 17:06:58 +0700

    fix: debug warning in frontend code

--- apps/frontend/src/components/Map.tsx
+++ apps/frontend/src/components/Map.tsx
@@ -1,5 +1,6 @@
-import { LatLngExpression } from 'leaflet';
+import { LatLngExpression, Map } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { Ref } from 'react';
 import { Circle, MapContainer, TileLayer, Tooltip } from 'react-leaflet';
 
 // metadata
@@ -7,30 +8,22 @@ const TILE_LAYER_ATTRIBUTION =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const TILE_LAYER_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-interface DeviceLocation {
-  id: string;
+type DeviceLocation = {
+  id: number;
   connected: boolean;
   location: LatLngExpression;
-}
+};
+
 interface MapProps {
-  setSelectedId: React.Dispatch<any>;
-  setMap: React.Dispatch<any>;
-  selectedId: string;
+  setMap: Ref<Map> | undefined;
   className: string;
   data: DeviceLocation[];
 }
 
-export const convertDevice = (object: any): DeviceLocation => {
-  return {
-    id: object.id,
-    connected: object.connected,
-    location: object.location,
-  };
-};
-
 export default function MapEngine(props: MapProps) {
   // mapdata
   const position = { lat: 10.5082062, lng: 106.8602405 };
+
   const zoom = 13;
 
   const data = props.data;
