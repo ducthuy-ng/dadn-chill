bb78f13c51a41d0cc7171aaae1b1fe492318fc6c
TuanKietTran <kyletran101.work@gmail.com>
2023-03-15 07:31:10 +0700

    Add sidebar

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -5,13 +5,16 @@ import { Navbar } from '../components';
 import NotificationsPage from '../pages/NotificationsPage';
 import HomePage from '../pages/HomePage';
 import SensorView from '../pages/SensorView';
-
-// import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import Sidebar from '../components/Sidebar';
+import { sidebar } from '../store';
+import { useAtom } from 'jotai';
 
 export function App() {
+  const [show] = useAtom(sidebar)
   return (
-    <div className="flex h-screen flex-col">
-      <Navbar />
+    <>
+      <Sidebar />
+
       {/* <br />
       <div role="navigation">
         <ul>
@@ -23,20 +26,23 @@ export function App() {
           </li>
         </ul>
       </div> */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/notifications" element={<NotificationsPage />}></Route>
-        <Route path="/sensor" element={<SensorView />} />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-    </div>
+      <div className={`flex h-screen flex-col${show ? " blur": ""}` }>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/notifications" element={<NotificationsPage />}></Route>
+          <Route path="/sensor" element={<SensorView />} />
+          <Route
+            path="/page-2"
+            element={
+              <div>
+                <Link to="/">Click here to go back to root page.</Link>
+              </div>
+            }
+          />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import { FaBars, FaBell } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useAtom } from 'jotai'
+import { sidebar } from '../store';
+
 
 const sensor = {
   name: 'Cảm biến 1',
@@ -8,46 +11,50 @@ const sensor = {
 };
 
 export function Navbar() {
+  const [show,setShow] = useAtom(sidebar)
   return (
     <div className="top-0 flex flex-row bg-white p-4 lg:items-center lg:justify-between">
-      <div className='flex items-center'>
-        <FaBars className="text-xl mr-5 h-5 w-5" />
-          <ol
-            // role="list"
-            className="flex max-w-2xl items-center space-x-2 "
-          >
-            {sensor.breadcrumbs.map((breadcrumb) => (
-              <li key={breadcrumb.id}>
-                <div className="flex items-center">
-                  <a
-                    href={breadcrumb.href}
-                    className="mr-2 text-sm font-medium text-gray-900"
-                  >
-                    {breadcrumb.name}
-                  </a>
-                  <svg
-                    width={16}
-                    height={20}
-                    viewBox="0 0 16 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                    className="h-5 w-4 text-gray-300"
-                  >
-                    <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
-                  </svg>
-                </div>
-              </li>
-            ))}
-            <li className="text-sm">
-              <a
-                href={sensor.href}
-                aria-current="page"
-                className="font-medium text-gray-500 hover:text-gray-600"
-              >
-                {sensor.name}
-              </a>
+      <div className="flex items-center">
+        <button
+          className="mr-5 h-5 w-5 text-xl"
+          type="button"
+          onClick = {() => setShow(!show)}
+        >
+          <FaBars />
+        </button>
+        <ol
+          // role="list"
+          className="flex max-w-2xl items-center space-x-2 "
+        >
+          {sensor.breadcrumbs.map((breadcrumb) => (
+            <li key={breadcrumb.id}>
+              <div className="flex items-center">
+                <a href={breadcrumb.href} className="mr-2 text-sm font-medium text-gray-900">
+                  {breadcrumb.name}
+                </a>
+                <svg
+                  width={16}
+                  height={20}
+                  viewBox="0 0 16 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="h-5 w-4 text-gray-300"
+                >
+                  <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                </svg>
+              </div>
             </li>
-          </ol>
+          ))}
+          <li className="text-sm">
+            <a
+              href={sensor.href}
+              aria-current="page"
+              className="font-medium text-gray-500 hover:text-gray-600"
+            >
+              {sensor.name}
+            </a>
+          </li>
+        </ol>
       </div>
       <div>
         <Link to={'/notifications'}>
