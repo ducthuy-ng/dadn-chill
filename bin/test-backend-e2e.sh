005d4618b6d214edf02ff5795c6cd4ebfdb84041
Thuy Nguyen <46134513+ducthuy-ng@users.noreply.github.com>
2023-03-24 23:02:33 +0700

    refactor: remove Kafka from dev (#26)
    * refactor: remove kafka from dev
    
    * refactor: remove unused workflow
    
    * bugfix: setup-dev
    
    * bugfix: create special CI/CD install

--- bin/test-backend-e2e.sh
+++ bin/test-backend-e2e.sh
@@ -4,7 +4,12 @@ set -eufx
 
 __cleanup() {
   docker compose -f ./docker-compose.dev.yaml down --volumes
-  exit $TEST_EXITCODE
+  kill $BACKEND_PID
+
+  # Wait for backend to exit cleanly
+  sleep 2
+
+  exit $E2E_EXITCODE
 }
 trap __cleanup EXIT
 
@@ -13,5 +18,9 @@ docker compose -f ./docker-compose.dev.yaml up -d
 # Waiting for container to stablize
 sleep 5
 
-npx nx test backend --verbose
-TEST_EXITCODE=$?
+npx nx serve backend &
+BACKEND_PID=$!
+sleep 3
+
+npx nx e2e backend-e2e
+E2E_EXITCODE=$?
