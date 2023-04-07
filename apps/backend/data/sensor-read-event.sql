7a39bb42a1e7005448e3eb5c1e2873bc1dc93703
ducthuy-ng <46134513+ducthuy-ng@users.noreply.github.com>
2023-04-07 20:48:07 +0700

    bugfix: update data to match SQL data schema

--- apps/backend/data/sensor-read-event.sql
+++ apps/backend/data/sensor-read-event.sql
@@ -3,9 +3,13 @@ CREATE SCHEMA IF NOT EXISTS data_pipeline;
 CREATE TABLE IF NOT EXISTS data_pipeline.sensor_read_event (
   event_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   sensor_id SERIAL NOT NULL,
-  read_ts VARCHAR(25),
+  read_ts VARCHAR(30),
   temperature REAL,
   humidity REAL,
   light_intensity REAL,
   earth_moisture REAL
 );
+
+COPY data_pipeline.sensor_read_event
+FROM
+  '/docker-entrypoint-initdb.d/sensor_read_events.csv' WITH (FORMAT CSV, HEADER TRUE);
